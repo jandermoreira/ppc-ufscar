e28e383d41a5b072062f8db8644edda2afa33c66
jandermoreira <moreira.jander@gmail.com>
2025-10-17 13:29:20 -0300

    Inclui sincronização com attrtoolbox

--- sincroniza-bia.sh
+++ sincroniza-bia.sh
@@ -2,8 +2,16 @@
 # Inicia o meld para os arquivos compartilhados
 # Jander, 2025
 
-files=$(echo ppc-ufscar.{sty,cls} ppc-ufscar-*.code.tex)
-for f in $files; do
-  argumentos=$(echo $argumentos --diff $f ~/graduacao/ppc-bia-2025/$f)
+# pcc-ufscar
+#arqsppc=$(echo ppc-ufscar.{sty,cls} ppc-ufscar-*.code.tex)
+#for f in $arqsppc; do
+#  argumentos=$(echo $argumentos --diff $f ~/graduacao/ppc-bia-2025/$f)
+#done
+
+# attrtoolbox
+arqsattr=attrtoolbox.sty
+for f in $arqsattr; do
+  argumentos=$(echo $argumentos --diff ~/texmf/tex/latex/local/attrtoolbox/$f ~/graduacao/ppc-bia-2025/$f)
 done
+
 eval meld $argumentos
